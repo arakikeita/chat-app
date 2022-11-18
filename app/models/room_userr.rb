d846d107068deddee28f601e8cffcb77ba600323
arakikeita <araki1728@gmail.com>
2022-11-19 08:09:45 +0900

    プルダウンの実装

--- app/models/room_userr.rb
+++
@@ -1,4 +0,0 @@
-class RoomUserr < ApplicationRecord
-  belong_to :room
-  belong_to :user
-end
--- db/migrate/20221118055407_create_room_users.rb
+++ db/migrate/20221118055407_create_room_users.rb
@@ -1,6 +1,6 @@
-class CreateRoomUserrs < ActiveRecord::Migration[6.0]
+class CreateRoomUsers < ActiveRecord::Migration[6.0]
   def change
-    create_table :room_userrs do |t|
+    create_table :room_users do |t|
       t.references :room, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
       t.timestamps
